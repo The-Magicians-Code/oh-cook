bc955dbeb92a7225de86fde7f78aab8f945ffd3c
Tanel Treuberg <tanetre@gmail.com>
2022-09-28 22:46:38 +0300

    Changes

---
+++ pytorch/ssdtorch/ssd300.py
@@ -0,0 +1,154 @@
+import torch
+torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+
+# List of available models in PyTorch Hub from Nvidia/DeepLearningExamples
+torch.hub.list('NVIDIA/DeepLearningExamples:torchhub')
+
+# load SSD model pretrained on COCO from Torch Hub
+precision = 'fp32'
+ssd300 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math=precision)
+
+# Sample images from the COCO validation set
+uris = [
+    'http://images.cocodataset.org/val2017/000000397133.jpg',
+    'http://images.cocodataset.org/val2017/000000037777.jpg',
+    'http://images.cocodataset.org/val2017/000000252219.jpg'
+]
+
+# For convenient and comprehensive formatting of input and output of the model, load a set of utility methods.
+utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd_processing_utils')
+
+# Format images to comply with the network input
+inputs = [utils.prepare_input(uri) for uri in uris]
+tensor = utils.prepare_tensor(inputs, False)
+
+# The model was trained on COCO dataset, which we need to access in order to
+# translate class IDs into object names.
+classes_to_labels = utils.get_coco_object_dictionary()
+
+# Next, we run object detection
+model = ssd300.eval().to("cuda")
+detections_batch = model(tensor)
+
+# By default, raw output from SSD network per input image contains 8732 boxes with
+# localization and class probability distribution.
+# Let’s filter this output to only get reasonable detections (confidence>40%) in a more comprehensive format.
+results_per_input = utils.decode_results(detections_batch)
+best_results_per_input = [utils.pick_best(results, 0.40) for results in results_per_input]
+
+
+
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+
+# The utility plots the images and predicted bounding boxes (with confidence scores).
+def plot_results(best_results):
+    for image_idx in range(len(best_results)):
+        fig, ax = plt.subplots(1)
+        # Show original, denormalized image...
+        image = inputs[image_idx] / 2 + 0.5
+        ax.imshow(image)
+        # ...with detections
+        bboxes, classes, confidences = best_results[image_idx]
+        for idx in range(len(bboxes)):
+            left, bot, right, top = bboxes[idx]
+            x, y, w, h = [val * 300 for val in [left, bot, right - left, top - bot]]
+            rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+            ax.text(x, y, "{} {:.0f}%".format(classes_to_labels[classes[idx] - 1], confidences[idx]*100), bbox=dict(facecolor='white', alpha=0.5))
+    # plt.show()
+        plt.savefig(f"results/f{image_idx}.png")
+    
+# Visualize results without Torch-TensorRT
+plot_results(best_results_per_input)
+
+
+
+import time
+import numpy as np
+
+import torch.backends.cudnn as cudnn
+cudnn.benchmark = True
+
+# Helper function to benchmark the model
+def benchmark(model, input_shape=(1024, 1, 32, 32), dtype='fp32', nwarmup=50, nruns=1000):
+    input_data = torch.randn(input_shape)
+    input_data = input_data.to("cuda")
+    if dtype=='fp16':
+        input_data = input_data.half()
+
+    print("Warm up ...")
+    with torch.no_grad():
+        for _ in range(nwarmup):
+            features = model(input_data)
+    torch.cuda.synchronize()
+    print("Start timing ...")
+    timings = []
+    with torch.no_grad():
+        for i in range(1, nruns+1):
+            start_time = time.time()
+            pred_loc, pred_label  = model(input_data)
+            torch.cuda.synchronize()
+            end_time = time.time()
+            timings.append(end_time - start_time)
+            if i%10==0:
+                print('Iteration %d/%d, avg batch time %.2f ms'%(i, nruns, np.mean(timings)*1000))
+
+    print("Input shape:", input_data.size())
+    print("Output location prediction size:", pred_loc.size())
+    print("Output label prediction size:", pred_label.size())
+    print('Average batch time: %.2f ms'%(np.mean(timings)*1000))
+    
+# Model benchmark without Torch-TensorRT
+model = ssd300.eval().to("cuda")
+benchmark(model, input_shape=(128, 3, 300, 300), nruns=100)
+
+
+
+model = ssd300.eval().to("cuda")
+traced_model = torch.jit.trace(model, [torch.randn((1,3,300,300)).to("cuda")])
+# This is just an example, and not required for the purposes of this demo
+torch.jit.save(traced_model, "ssd_300_traced.jit.pt")
+# Obtain the average time taken by a batch of input with Torchscript compiled modules
+benchmark(traced_model, input_shape=(128, 3, 300, 300), nruns=100)
+
+
+
+import torch_tensorrt
+
+# The compiled module will have precision as specified by "op_precision".
+# Here, it will have FP16 precision.
+trt_model = torch_tensorrt.compile(traced_model,
+    inputs= [torch_tensorrt.Input((3, 3, 300, 300), dtype=torch.half)],
+    enabled_precisions= {torch.half}, # Run with FP16
+    workspace_size= 1 << 20
+)
+
+
+
+# using a Torch-TensorRT module is exactly the same as how we usually do inference in PyTorch i.e. model(inputs)
+detections_batch = trt_model(tensor.to(torch.half)) # convert the input to half precision
+
+# By default, raw output from SSD network per input image contains 8732 boxes with
+# localization and class probability distribution.
+# Let’s filter this output to only get reasonable detections (confidence>40%) in a more comprehensive format.
+results_per_input = utils.decode_results(detections_batch)
+best_results_per_input_trt = [utils.pick_best(results, 0.40) for results in results_per_input]
+
+
+
+# Visualize results with Torch-TensorRT
+plot_results(best_results_per_input_trt)
+
+
+
+batch_size = 128
+
+# Recompiling with batch_size we use for evaluating performance
+trt_model = torch_tensorrt.compile(traced_model,
+    inputs = [torch_tensorrt.Input((batch_size, 3, 300, 300), dtype=torch.half)],
+    enabled_precisions= {torch.half}, # Run with FP16
+    workspace_size= 1 << 20
+)
+
+benchmark(trt_model, input_shape=(batch_size, 3, 300, 300), dtype='fp16', nruns=100)
