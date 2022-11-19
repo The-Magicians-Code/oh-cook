532be0531b1967efaa817d26b824e324880e8aaf
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-11-19 23:59:48 +0200

    Update run_dock.sh

--- yolorun/run_dock.sh
+++ yolorun/run_dock.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# git clone https://github.com/ultralytics/yolov5
 docker build -t cont2 .
 docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v $PWD/yolov5:/workspace -p 3001:3001 cont2
 # docker run -it --rm -p 3000:3000 cont1
