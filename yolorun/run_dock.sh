a292f02ae45709d6103a66214a9d38efc302ff1f
Tanel Treuberg <tanetre@gmail.com>
2024-01-01 18:12:34 +0200

    Clearing from desktop PC

--- yolorun/run_dock.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker build -t cont2 .
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v $PWD/userexports:/usr/src/app/hostitems -p 3001:3001 cont2
